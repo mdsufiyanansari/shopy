e932df501c53a72edd521805badf2aaf169666f6
mdsufiyanansari <mdsufiyanansari812002@gmail.com>
2024-08-07 19:25:22 +0530

    shopy

---
+++ src/pages/Product.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <>
+    <div className="h-screen center text-4xl">
+    <h1>PRODUCT DETAILS .....</h1>
+    </div>
+    </>
+  )
+}
+
+export default Product
